dbd7f3f009a0729c70fefa9dd8eec48e56639a65
Eric PREVOTEAU <eric.prevoteau@ac-strasbourg.fr>
2022-12-27 09:59:53 +0100

    add several v1.8 packet regnum (to check)

--- comfortzone_decoder.cpp
+++ comfortzone_decoder.cpp
@@ -6,6 +6,7 @@
 
 static czdec::KNOWN_REGISTER kr_decoder[] =
 	{
+#if HP_PROTOCOL == HP_PROTOCOL_1_6
 		// don't know why but extra hot water off does not use the same message as extra hot water on
 		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x07, 0x41, 0x19, 0x00}, czcraft::KR_EXTRA_HOT_WATER_OFF, "Extra hot water - off", czdec::cmd_r_generic, czdec::cmd_w_extra_hot_water, czdec::reply_r_extra_hot_water, czdec::reply_w_generic},
 		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x07, 0x81, 0x19, 0x00}, czcraft::KR_EXTRA_HOT_WATER_ON, "Extra hot water - on", czdec::cmd_r_generic, czdec::cmd_w_extra_hot_water, czdec::reply_r_extra_hot_water, czdec::reply_w_generic},
@@ -74,7 +75,84 @@ static czdec::KNOWN_REGISTER kr_decoder[] =
 
 		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x07, 0x05, 0x00, 0x00}, czcraft::KR_UNCRAFTABLE, "Status 08", czdec::cmd_r_generic, czdec::empty, czdec::reply_r_status_08, czdec::reply_w_generic},	// 0x50 bytes
 
+#endif
 
+#if HP_PROTOCOL == HP_PROTOCOL_1_8
+// status frame
+// not changed when hot water settings are modified
+// query 01 02 03 04 0B 09 00 00 00 16 ..  => 17
+// 01 02 03 04 0B 09 00 00 00 ?? Status: reply: 41 44 44 52 07 8A 65 6F DE 02 2C 72 01 02 03 04 0B 09 00 00 00 CA 8E 72 42 AE 34 06 1F FF 00 00 11 03 C2 51 41 2B 01 40 61 00 00 F8  => 2C
+//                                              41 44 44 52 07 8A 65 6F DE 02 2C 72 01 02 03 04 0B 09 00 00 00 CA 8E 72 42 AE 34 06 1F FF 00 00 11 03 C2 51 41 2B 01 40 61 00 00 F8  => 2C
+//                                              41 44 44 52 07 8A 65 6F DE 02 2C 72 01 02 03 04 0B 09 00 00 00 CA 8E 72 42 AE 34 06 1F FF 00 00 11 03 C2 51 41 2B 01 40 61 00 00 F8  => 2C
+//                                              41 44 44 52 07 8A 65 6F DE 02 2C 72 01 02 03 04 0B 09 00 00 00 CA 8E 72 42 AE 34 06 1F FF 00 00 11 03 C2 51 41 2B 01 40 61 00 00 F8  => 2C
+//                                              41 44 44 52 07 8A 65 6F DE 02 2C 72 01 02 03 04 0B 09 00 00 00 CA 8E 72 42 AE 34 06 1F FF 00 00 11 03 C2 51 41 2B 01 40 61 00 00 F8  => 2C
+//                                              
+
+
+
+// invalid frame query: 
+// query 01 02 03 04 0B 09 00 00 00 B1 ..  => 17   ... no reply from heatpump
+
+
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x0B, 0x04}, czcraft::KR_UNCRAFTABLE, "Hot water extra setting (not supported)", czdec::cmd_r_generic, czdec::cmd_w_temp, czdec::reply_r_temp, czdec::reply_w_generic},
+
+// Write: 01 02 03 04 0B 09 00 0D 04 B4 00 ..  => 18  Reply: 01 02 03 04 0B 09 00 0D 04 00 ..  => 17
+// means 18.0°C ?
+// Read:  01 02 03 04 0B 09 00 0D 04 02 ..  => 17     Reply: 01 02 03 04 0B 09 00 0D 04 B4 00 ..  => 18
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x0D, 0x04}, czcraft::KR_ROOM_HEATING_TEMP, "Heating calculated setting", czdec::cmd_r_generic, czdec::cmd_w_temp, czdec::reply_r_temp, czdec::reply_w_generic},
+
+
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x0E, 0x00}, czcraft::KR_UNCRAFTABLE, "Sanitary priority (get)", czdec::cmd_r_generic, czdec::empty, czdec::reply_r_sanitary_priority, czdec::empty},
+
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x25, 0x01}, czcraft::KR_UNCRAFTABLE, "Hot water extra time (not supported)", czdec::cmd_r_generic, czdec::cmd_w_time, czdec::reply_r_time, czdec::reply_w_generic},
+// write: 01 02 03 04 0B 09 00 25 01 04 ..  => 17   Reply: 01 02 03 04 0B 09 00 25 01 3C 00 00 00 ..  => 1A
+// write: 01 02 03 04 0B 09 00 25 01 3D ..  => 17   Reply: 01 02 03 04 0B 09 00 25 01 3D ..  => 17
+// read:  01 02 03 04 0B 09 00 25 01 01 ..  => 17   Reply: 01 02 03 04 0B 09 00 25 01 3D ..  => 17
+// write: 01 02 03 04 0B 09 00 25 01 3C ..  => 17   Reply: 01 02 03 04 0B 09 00 25 01 3C ..  => 17
+
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x1A, 0x04}, czcraft::KR_FAN_SPEED, "Fan speed", czdec::cmd_r_generic, czdec::cmd_w_fan_speed, czdec::reply_r_fan_speed, czdec::reply_w_generic},
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x7D, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR0, "Hardware Settings - Adjustments - TE0 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x7E, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR1, "Hardware Settings - Adjustments - TE1 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x7F, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR2, "Hardware Settings - Adjustments - TE2 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x80, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR3, "Hardware Settings - Adjustments - TE3 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x81, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR4, "Hardware Settings - Adjustments - TE4 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x82, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR5, "Hardware Settings - Adjustments - TE5 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x83, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR6, "Hardware Settings - Adjustments - TE6 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},	// not tested
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x84, 0x00}, czcraft::KR_TEMP_OFFSET_SENSOR7, "Hardware Settings - Adjustments - TE7 Adjust", czdec::cmd_r_generic, czdec::cmd_w_temp_1byte, czdec::reply_r_temp_1byte, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x92, 0x03}, czcraft::KR_HOT_WATER_TEMP, "Hot water calculated setting", czdec::cmd_r_generic, czdec::cmd_w_temp, czdec::reply_r_temp, czdec::reply_w_generic},
+
+
+
+// read                                             reply: 01 02 03 04 0B 09 00 92 03 1D 02 ..  => 18 
+
+
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x00, 0x9F, 0x02}, czcraft::KR_LED_LUMINOSITY, "LED luminosity", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x01, 0x04, 0x00}, czcraft::KR_HOUR, "Hour", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x01, 0x05, 0x00}, czcraft::KR_MINUTE, "Minute", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+// 01 02 03 04 0B 09 01 06 00 ?? KR_SECOND ?
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x01, 0x07, 0x00}, czcraft::KR_DAY, "Day of Month", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x01, 0x08, 0x00}, czcraft::KR_MONTH, "Month (1=Jan)", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x01, 0x09, 0x00}, czcraft::KR_YEAR, "Year (20xx)", czdec::cmd_r_generic, czdec::cmd_w_digit, czdec::reply_r_digit, czdec::reply_w_generic},
+// 01 02 03 04 0B 09 01 47 01 ??
+
+// 01 02 03 04 0B 09 03 6F 0E ??
+// 01 02 03 04 0B 09 04 9C 3E ??
+
+//  Hot water extra: on/off
+//        01 02 03 04 0B 09 41 17 00 FE FF ..  => 18    reply: 41 44 44 52 07 8A 65 6F DE 02 17 77 01 02 03 04 0B 09 41 17 00 6E ..  => 17
+// write: 01 02 03 04 0B 09 41 17 00 FF FE ..  => 18    reply: 41 44 44 52 07 8A 65 6F DE 02 17 77 01 02 03 04 0B 09 41 17 00 4A ..  => 17
+// read:  01 02 03 04 0B 09 01 17 00 02 ..  => 17       reply: 41 44 44 52 07 8A 65 6F DE 02 18 72 01 02 03 04 0B 09 01 17 00 08 4A ..  => 18
+		{ {0x01, 0x02, 0x03, 0x04, 0x0B, 0x09, 0x41, 0x17, 0x00}, czcraft::KR_EXTRA_HOT_WATER_OFF, "Extra hot water - off", czdec::cmd_r_generic, czdec::cmd_w_extra_hot_water, czdec::reply_r_extra_hot_water, czdec::reply_w_generic},
+
+
+
+
+#endif
 		{ {0}, czcraft::KR_UNCRAFTABLE, NULL, NULL, NULL, NULL, NULL}
 	};
 
@@ -173,6 +251,6 @@ void czdec::dump_frame(comfortzone_heatpump *cz_class, const char *prefix)
 	}
 
 	DPRINT(" => ");
-	DPRINT(cz_class->cz_size, HEX);
+	DPRINTLN(cz_class->cz_size, HEX);
 }
 
