0ed81fcaf0f6bceabcc557138eff2d3c1012a88c
Eric PREVOTEAU <eric.prevoteau@ac-strasbourg.fr>
2023-01-11 09:09:02 +0100

    add comment into configuration file

--- comfortzone_config.h
+++ comfortzone_config.h
@@ -1,14 +1,18 @@
 #ifndef _COMFORTZONE_CONFIG_H
 #define _COMFORTZONE_CONFIG_H
 
-#define HP_PROTOCOL_1_6 16
-#define HP_PROTOCOL_1_8 18
+#define HP_PROTOCOL_1_6 160
+#define HP_PROTOCOL_1_8 180
 
+// adjust HP_PROTOCOL value according to protocol version to use
 #define HP_PROTOCOL HP_PROTOCOL_1_8
 
 #undef DEBUG
+// uncomment to enable debug mode
+//#define DEBUG
 
 // Serial console
+// uncomment and set to serial port where debug data should be sent
 //#define OUTSER Serial
 
 #define COMFORTZONE_HEATPUMP_LAST_MESSAGE_BUFFER_SIZE 256
