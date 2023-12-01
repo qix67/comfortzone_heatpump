d51668d6ada428b08db08ae83ad7616f819416f4
Eric PREVOTEAU <eric.prevoteau@ac-strasbourg.fr>
2023-12-01 12:02:07 +0100

    Fix compilation problem with esp32 on arduino
    issue #12

--- platform_specific.h
+++ platform_specific.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #ifdef ESP_PLATFORM
+#ifndef ARDUINO
 #include "esp_timer.h"
 inline unsigned long millis() {
 	return esp_timer_get_time() / 1000;
 }
+#endif
 #endif
 
 #ifndef NULL
